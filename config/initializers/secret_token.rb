fd4a7a3877784ebd1bc5e39243aa2cdc013c5fd5
Brad Wilcox <brad_wilcox@me.com>
2013-07-31 12:43:15 -0400

    converted html.erb to haml in application views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NgEntrepernology::Application.config.secret_key_base = '69177ac1973668dde246c0f35ab1bb2584b485bd5c9ef64e94714a3d70292cf1e14d7bc31adb921fe60c1a6912b34c2bc0455b0d2baab76a46283b24ba025dcc'
